e43805619f4fb21700cd9bd86863f11135e816fe
t4h4wk <ttahawk@gmail.com>
2013-03-03 20:51:58 +0100

    modified:   megasync.py upload_all_files

--- megasync.py
+++ megasync.py
@@ -14,6 +14,8 @@ import stat
 import tempfile
 import time
 
+directorio_local = 'temp'
+
 # Devuelve la url remota de los ficheros que le pasamos en hash
 def getpath(files, hash):
 	hash2path = {}
@@ -40,7 +42,7 @@ def download_all_files():
     for file in files.items():
       if 'meta_mac' in file[1]:
 	remote_filepath = getpath(files, file[1]['h'])
-	local_filepath = 'temp' + remote_filepath
+	local_filepath = directorio_local + remote_filepath
 	# Descargamos el fichero si no existe en local
 	if not os.path.exists(local_filepath):
 	  verify_local_path(local_filepath)
@@ -58,12 +60,32 @@ def download_all_files():
       print '[ERROR] Ha ocurrido algun error al descargar los ficheros'
     return result
     
-# Sube todo el arbol de ficheros locales a Mega
+# Sube todo el arbol de ficheros locales a Mega. De momento los directorios deben existir
 def upload_all_files():
-  result = True
-  
-  client.uploadfile('temp/Cloud Drive/file.txt', 2, 'file.txt')
-  return result
+    result = True
+    
+    # Lo primero que habria que hacer es recorrer el arbol de directorios locales
+    # y comprobar si existen en remoto para crear los que no existan.
+    
+    
+    # Recorremos el arbol de directorios locales
+    for path, dir, name in os.walk(directorio_local):
+      for n in name:
+	# Guardamos en variables los diferentes path's y nombres de fichero
+	local_filepath = path + '/' + n
+	remote_filename = n
+	remote_filepath = path.replace(directorio_local, '')
+	# Para subir un fichero necesitamos saber el hash del directorio remoto donde se subira
+	hash_path = path2hash(remote_filepath)
+	# Si no existe el directorio en Mega hay que crearlo. POR HACER
+	if hash_path == 'null':
+	  print 'ERROR: No existe el directorio ' + remote_filepath + ' en Mega'
+	  return False
+	# Subimos el fichero
+	print '[*] Subiendo ' + local_filepath
+	client.uploadfile(local_filepath, files[hash_path]['h'], remote_filename)
+	
+    return result
 
 # Verifica si un directorio existe en local y si no existe lo crea
 def verify_local_path(path):
@@ -93,14 +115,18 @@ def show_files(path):
     for n in name:
       print '\t' + path + '/' + n
 
+# Funcion que devuelve el path de un hash
+def path2hash(path):
+    hash = 'null'
+    for f in files.items():
+      if getpath(files, f[0]) == path:
+	hash = f[0]
+	
+    return hash
 
 # Main. Pide el email y el password del usuario de Mega      
 if __name__ == '__main__':
-
-    show_folders('temp')
-    show_files('temp')
     
-"""
     email = raw_input("Email [%s]: " % getpass.getuser())
     if not email:
         email = getpass.getuser()
@@ -112,7 +138,7 @@ if __name__ == '__main__':
     print files
     print
     print
-    download_all_files()
+    #download_all_files()
     upload_all_files()
-"""
+    
 
